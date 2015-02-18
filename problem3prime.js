f3d4981101a417de220bd16b15739fd20f7ccd59
Thomas Bae <thomas.bae0@gmail.com>
2015-02-18 11:44:49 -0500

    commenting practice problem 3

--- problem3prime.js
+++ problem3prime.js
@@ -7,13 +7,14 @@ var primeNumbers = function(number){
     // run the calculation for sequence until input number is reached 
     for(var x=2; x <= number; x++){
         
+        //determine if given number is divisble by X
         if(number % x === 0){
-            //console.log(x);
+            //check every number and see if given number is divisible 
             for(var y = 2; y < Math.sqrt(x); y++){
               //  console.log(x);
             if(number % y === 0){
                 //console.log(x);
-            
+            //basic return if number is not prime factor
             return false;
              }  
              else 
@@ -27,6 +28,7 @@ var primeNumbers = function(number){
     }
 
 };
-
+//call the function
 primeNumbers(600851475143);
+//log the result 
 console.log(largePrimeFactor);
